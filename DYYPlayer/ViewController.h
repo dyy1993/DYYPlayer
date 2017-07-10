a2e0e0b0e6f917418524eca995bbd959c7c6bc71
dyy <771750834@qq.com>
2017-07-10 11:30:40 +0800

    init

---
+++ DYYPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYYPlayer
+//
+//  Created by yang on 2017/7/10.
+//  Copyright © 2017年 dingyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
